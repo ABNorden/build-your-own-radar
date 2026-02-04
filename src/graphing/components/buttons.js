650d656a2f2c5972ef17a72187ef57ff62eff8ac
ABNorden <adrian-borowski@outlook.de>
2026-02-04 13:05:15 +0100

    Update buttons.js

--- src/graphing/components/buttons.js
+++ src/graphing/components/buttons.js
@@ -4,14 +4,14 @@ function renderButtons(radarFooter) {
   buttonsRow
     .append('button')
     .classed('buttons__wave-btn', true)
-    .text('Print this Radar')
+    .text('Trendradar drucken')
     .on('click', window.print.bind(window))
 
   buttonsRow
     .append('a')
     .classed('buttons__flamingo-btn', true)
     .attr('href', window.location.href.substring(0, window.location.href.indexOf(window.location.search)))
-    .text('Generate new Radar')
+    .text('Neues Radar erstellen')
 }
 
 module.exports = {
