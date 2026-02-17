af25fc3c1479786ef00feb5e5738b7804a7b56a2
ABNorden <adrian-borowski@outlook.de>
2026-02-17 14:01:16 +0100

    Update banner.js

--- src/graphing/components/banner.js
+++ src/graphing/components/banner.js
@@ -9,7 +9,7 @@ function renderBanner(renderFullRadar) {
     const documentTitle = document.title[0].toUpperCase() + document.title.slice(1)
 
     document.title = documentTitle
-    d3.select('.hero-banner__wrapper').append('p').classed('hero-banner__subtitle-text', true).text(document.title)
+   // d3.select('.hero-banner__wrapper').append('p').classed('hero-banner__subtitle-text', true).text(document.title)
     d3.select('.hero-banner__title-text').on('click', renderFullRadar)
 
     addPdfCoverTitle(documentTitle)
