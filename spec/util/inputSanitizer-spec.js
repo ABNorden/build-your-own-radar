af0589c1d5442575cdb3225df39bdf8741e65aae
ABNorden <adrian-borowski@outlook.de>
2026-03-23 12:51:20 +0100

    Update inputSanitizer-spec.js

--- spec/util/inputSanitizer-spec.js
+++ spec/util/inputSanitizer-spec.js
@@ -12,6 +12,7 @@ describe('InputSanitizer', function () {
       ring: '<a href="/asd">Adopt</a>',
       quadrant: '<strong>techniques and tools</strong>',
       isNew: 'true<br>',
+      Bedeutung: "<p>Relevant <script>alert('dangerous');</script><strong>Info</strong></p>",
     }
 
     blip = sanitizer.sanitize(rawBlip)
@@ -36,6 +37,11 @@ describe('InputSanitizer', function () {
   it('strips out all tags from blip quadrant', function () {
     expect(blip.quadrant).toEqual('techniques and tools')
   })
+  
+    it('sanitizes the optional Bedeutung field', function () {
+    expect(blip.bedeutung).toEqual('<p>Relevant <strong>Info</strong></p>')
+  })
+  
 
   it('trims white spaces in keys and values', function () {
     rawBlip = {
@@ -53,7 +59,7 @@ describe('Input Santizer for Protected sheet', function () {
   var sanitizer, rawBlip, blip, header
   beforeAll(function () {
     sanitizer = new InputSanitizer()
-    header = ['name', 'quadrant', 'ring', 'isNew', 'description',]
+    header = ['name', 'quadrant', 'ring', 'isNew', 'description', 'Bedeutung']
 
     rawBlip = [
       "Hello <script>alert('dangerous');</script>there <h1>blip</h1>",
@@ -61,6 +67,7 @@ describe('Input Santizer for Protected sheet', function () {
       "<a href='/asd'>Adopt</a>",
       'true<br>',
       "<b>Hello</b> <script>alert('dangerous');</script>there <h1>heading</h1>",
+      "<p>Useful <script>alert('dangerous');</script><strong>context</strong></p>",
     ]
 
     blip = sanitizer.sanitizeForProtectedSheet(rawBlip, header)
@@ -86,6 +93,10 @@ describe('Input Santizer for Protected sheet', function () {
     expect(blip.quadrant).toEqual('techniques & tools')
   })
 
+   it('sanitizes the optional Bedeutung field', function () {
+    expect(blip.bedeutung).toEqual('<p>Useful <strong>context</strong></p>')
+  })
+  
   it('trims white spaces in keys and values', function () {
     rawBlip = {
       ' name': '   Some name ',
@@ -108,6 +119,7 @@ describe('Input Santizer for Protected sheet', function () {
       quadrant: '',
       isNew: '',
       status: '',
+      bedeutung: '',
     })
   })
 })
