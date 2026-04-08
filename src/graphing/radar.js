7a8bc9b8b111251293d00793fa6b9e7b1d85129a
ABNorden <adrian-borowski@outlook.de>
2026-04-08 10:51:13 +0200

    Update radar.js

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -8,7 +8,7 @@ const AutoComplete = require('../util/autoComplete')
 const config = require('../config')
 const featureToggles = config().featureToggles
 const { plotRadarBlips } = require('./blips')
-const { graphConfig, getGraphSize } = require('./config')
+const { graphConfig, getGraphSize, getInnerWidth } = require('./config')
 
 const { renderBanner } = require('./components/banner')
 const { renderQuadrantSubnav } = require('./components/quadrantSubnav')
@@ -687,8 +687,8 @@ const Radar = function (size, radar) {
     var translateXAll = (((1 - adjustX) / 2) * size * scale) / 2 + ((1 - adjustX) / 2) * (1 - scale / 2) * size
     var translateYAll = (((1 + adjustY) / 2) * size * scale) / 2
 
-    var moveRight = ((1 + adjustX) * (0.8 * window.innerWidth - size)) / 2
-    var moveLeft = ((1 - adjustX) * (0.8 * window.innerWidth - size)) / 2
+    var moveRight = ((1 + adjustX) * (0.8 * getInnerWidth() - size)) / 2
+    var moveLeft = ((1 - adjustX) * (0.8 * getInnerWidth() - size)) / 2
 
     var blipScale = 3 / 4
     var blipTranslate = (1 - blipScale) / blipScale
